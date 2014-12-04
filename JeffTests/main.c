2ed9ff5e9ef0300236d3840dcc4c809f08fcf599
RickyLarsen <rmlarsen83@gmail.com>
2014-12-03 17:56:52 -0700

    Modified the windows to display the correct options in fight or buy

--- JeffTests/main.c
+++ JeffTests/main.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-#include <curses.h>
-
+#include "curses.h"
+#include "character.h"
+ 
 #define WIDTH 30
 #define HEIGHT 10 
 
 int startx = 0;
 int starty = 0;
 
-char *choices[] = { 
-			"Choice 1",
-			"Choice 2",
-			"Choice 3",
-			"Choice 4",
-			"Exit",
-		  };
-int n_choices = sizeof(choices) / sizeof(char *);
+const char *choices[4];
+int n_choices;
 void print_menu(WINDOW *menu_win, int highlight);
+void setOptions(character*);
+void fight(character*);
+void buy(character*);
 
-int main()
-{	WINDOW *menu_win;
+void fight(character* enemy) {
+	setOptions(enemy);
+	WINDOW *menu_win;
 	int highlight = 1;
 	int choice = 0;
 	int c;
@@ -29,7 +27,7 @@ int main()
 	cbreak();	/* Line buffering disabled. pass on everything */
 	startx = (80 - WIDTH) / 2;
 	starty = (24 - HEIGHT) / 2;
-		
+	
 	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
 	keypad(menu_win, TRUE);
 	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
@@ -55,41 +53,72 @@ int main()
 			case 10:
 				choice = highlight;
 				break;
-			default:
-				mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
-				refresh();
+		}
+		print_menu(menu_win, highlight);
+		if(choice != 0)	/* User did a choice come out of the infinite loop */
+			break;
+	}
+	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	clrtoeol();
+	refresh();
+	getch();
+	endwin();
+}
+
+void buy(character* vendor) {
+	setOptions(vendor);
+	WINDOW *menu_win;
+	int highlight = 1;
+	int choice = 0;
+	int c;
+
+	initscr();
+	clear();
+	noecho();
+	cbreak();	/* Line buffering disabled. pass on everything */
+	startx = (80 - WIDTH) / 2;
+	starty = (24 - HEIGHT) / 2;
+	
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE);
+	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+	refresh();
+	print_menu(menu_win, highlight);
+	//char input;
+	//while (input == 'y') {
+	while(1)
+	{	c = wgetch(menu_win);
+		switch(c)
+		{	case KEY_UP:
+				if(highlight == 1)
+					highlight = n_choices;
+				else
+					--highlight;
+				break;
+			case KEY_DOWN:
+				if(highlight == n_choices)
+					highlight = 1;
+				else 
+					++highlight;
+				break;
+			case 10:
+				choice = highlight;
 				break;
 		}
 		print_menu(menu_win, highlight);
 		if(choice != 0)	/* User did a choice come out of the infinite loop */
 			break;
 	}
-	//int cost = ((vendor->rockGrade - 1) * 100); 
-	//if (player->money > cost) { // see if they have enough money
-		// subtract from the user's money the cost of the weapon
-		// Update the user's weapon to the one that has been bought
-	//} else { 
-		// Not enough money
-		// Print not enough money.
-		
-	//}
-	
-	//input = wgetch(menu_win);
-
-	//}	
 	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
 	clrtoeol();
 	refresh();
 	getch();
 	endwin();
-	return 0;
 }
-
 
 void print_menu(WINDOW *menu_win, int highlight)
 {
-	int x, y, i;	
-
+	int x, y, i;
 	x = 2;
 	y = 2;
 	box(menu_win, 0, 0);
@@ -104,4 +133,31 @@ void print_menu(WINDOW *menu_win, int highlight)
 		++y;
 	}
 	wrefresh(menu_win);
+}
+
+void setOptions(character* options) {
+  int index = 0;
+  const char * rock = options->rockName.c_str();
+  const char * paper = options->paperName.c_str();
+  const char * scissors = options->scissorsName.c_str();
+  if(options->type == "Vendor") {
+	n_choices = 4;
+	choices[index] = rock;
+	index++;
+	choices[index] = paper;
+	index++;
+	choices[index] = scissors;
+	index++;
+	choices[index] = "Exit";
+  }
+
+  if(options->type == "Player") {
+	n_choices = 3;
+    choices[index] = rock;
+	index++;
+	choices[index] = paper;
+	index++;
+	choices[index] = scissors;
+
+  }
 }

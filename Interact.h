b83d13f52ac9f3e53aeab3b058c4a5db45f1fda9
RickyLarsen <rmlarsen83@gmail.com>
2014-12-10 13:17:10 -0700

    Fight done, buggy still

--- Interact.h
+++ Interact.h
@@ -1,10 +1,31 @@
-//Copyright Jeff Hammond/ Richard Larsen 2014
+#include "curses.h"
+#include "character.h"
+#include <sstream>
+#include <string>
+using namespace std;
+ 
+#define MENU_WIDTH 30
+#define MENU_HEIGHT 10
+
+#define RESULT_WIDTH 80
+#define RESULT_HEIGHT 10
+
+int tugOfWarBar = 0;
+int startx = 0;
+int starty = 0;
+
+string choices[4];
+int n_choices;
 
 class Interact {
+
+public:
   int fightLogic(int userGrade, int winner, int enemyGrade);
   void setOptions(character*);
-  void fight(character*);
-  void buy(character*);
-  int getDecision(character*);
+  void fight(WINDOW*, character*, int);
+  void buy(character*, int);
+  void getDecision(character*);
+  void print_menu(WINDOW *menu_win, int highlight);
+  void print_results(WINDOW *result_win);
 };
 
--- interact.cpp
+++ interact.cpp
@@ -1,71 +1,86 @@
+//Copyright Jeff Hammond/ Richard Larsen 2014
+
 #include "curses.h"
-#include "character.h"
 #include "Interact.h"
 #include <sstream>
- 
-#define WIDTH 30
-#define HEIGHT 10
 
-int startx = 0;
-int starty = 0;
+void Interact::getDecision(character* person) {
+  setOptions(person);
+  WINDOW *menu_win;
+  WINDOW *result_win;
+  int highlight = 1;
+  int choice = 0;
+  int c;
+  int winwidth = 0;
+  int winheight = 0;
 
-string choices[4];
-int n_choices;
-void print_menu(WINDOW *menu_win, int highlight);
+  initscr();
+  clear();
+  noecho();
+  cbreak();	/* Line buffering disabled. pass on everything */
+  getmaxyx(stdscr, winheight, winwidth);
+  startx = (winwidth - MENU_WIDTH);
+  starty = (winheight - MENU_HEIGHT);
 
+  menu_win = newwin(MENU_HEIGHT, MENU_WIDTH, starty, startx);
+  startx = (winwidth - RESULT_WIDTH);
+  starty = (winheight - RESULT_HEIGHT);
+  result_win = newwin(RESULT_HEIGHT,RESULT_WIDTH,starty,startx);
 
-int Interact::getDecision(character* person) {
-  	setOptions(person);
-	WINDOW *menu_win;
-	int highlight = 1;
-	int choice = 0;
-	int c;
-	int winwidth = 0;
-	int winheight = 0;
-	
-	initscr();
-	clear();
-	noecho();
-	cbreak();	/* Line buffering disabled. pass on everything */
-	getmaxyx(stdscr, winheight, winwidth);
-	startx = (winwidth - WIDTH);
-	starty = (winheight - HEIGHT);
-	
-	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
-	keypad(menu_win, TRUE);
-	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
-	refresh();
-	print_menu(menu_win, highlight);
-	//char input;
-	//while (input == 'y') {
-	while(1)
-	{	c = wgetch(menu_win);
-		switch(c)
-		{	case KEY_UP:
-				if(highlight == 1)
-					highlight = n_choices;
-				else
-					--highlight;
-				break;
-			case KEY_DOWN:
-				if(highlight == n_choices)
-					highlight = 1;
-				else 
-					++highlight;
-				break;
-			case 10:
-				choice = highlight;
-				break;
-		}
-		print_menu(menu_win, highlight);
-		if(choice != 0)	/* User did a choice come out of the infinite loop */
-			break;
-	}
-	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
-	clrtoeol();
-	refresh();
-	getch();
-	endwin();
+  keypad(menu_win, TRUE);
+  //mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+  //refresh();
+
+  print_results(result_win);
+  print_menu(menu_win, highlight);
+
+  while(tugOfWarBar > -6 && tugOfWarBar < 6 && choice != 4) {
+    c = wgetch(menu_win);
+	choice = 0;
+    switch(c) {	
+	  case KEY_UP:
+	  case 'w':
+	  case 'k':
+        if(highlight == 1)
+          highlight = n_choices;
+        else
+          --highlight;
+        break;
+	  case KEY_DOWN:
+	  case 'j':
+	  case 's':
+        if(highlight == n_choices)
+          highlight = 1;
+        else 
+          ++highlight;
+        break;
+      case 10:
+        choice = highlight;
+        break;
+    }
+    print_menu(menu_win, highlight);
+    if(choice != 0)	{/* User did a choice come out of the infinite loop */
+      wclear(result_win);
+	  wrefresh(result_win);
+	  if (n_choices == 4) {
+//        buy(person, choice);
+        //this will continue to be called until choice == 4
+      } else {
+        fight(result_win, person, choice);
+        //this will continue to be called until player or enemy wins
+      }
+    
+    }
+  }
+  if (tugOfWarBar <= -6) {}
+  else if (tugOfWarBar >= 6) 
+    person->encounterDone++;
+  
+  clrtoeol();
+  refresh();
+  getch();
+  endwin();
+
 }
 
 int Interact::fightLogic(int userGrade, int winner, int enemyGrade) {
@@ -82,50 +97,70 @@ int Interact::fightLogic(int userGrade, int winner, int enemyGrade) {
 	return result;
 }
 
-void Interact::fight(character* enemy) {
-  int tugOfWarBar = 0;
+void Interact::fight(WINDOW *result_win, character* enemy, int choice) {
+  int rock = 0;
+  int paper = 1;
+  int scissors = 2;
+  int p1, p2;
+  int playerGrade;
+  string playerWeap;
+  int winner = 0;
 
-  // Put weapons for player in an array.
-
-  // Put weapns for enemy in an array.
-
-  // Display players weapons
-
-  // Display enemy weapons
-
-  // Begin while loop while player or enemy hit points do not equal zero
-  while(tugOfWarBar < -6 && tugOfWarBar > 6) {
-    int rock = 0;
-	int paper = 1;
-	int scissors = 2;
-	int p1, p2;
-
-	//Players choose
-
-	int winner = (p1 - p2 + 3) % 3;
-	tugOfWarBar += fightLogic(/*player->rockpaperscissorsgrade*/, winner, /*enemy->rockpaperscissorsgrade*/);
-    
+  if (choice == 1) {// this means player chose rock
+    p1 = rock;
+	playerGrade = player->rockGrade;
+	playerWeap = player->rockName;
+  } else if (choice == 2) { // this means player chose paper
+	p1 = paper;
+	playerGrade = player->paperGrade;
+	playerWeap = player->paperName;
+  } else {  // player chose scissors
+	p1 = scissors;
+	playerGrade = player->scissorsGrade;
+	playerWeap = player->scissorsName;
   }
-  // Get users selection for weapon to use
 
-  // Randomly generat which weapon the enemy will use
+  if (enemy->rockGrade == 0 && enemy->paperGrade == 0) { // it is an enemy with only scissors
 
-  // Compare the weapons used between the player and the enemy
+	p2 = scissors;
+	winner = (p1 - p2 + 3) % 3;
+	tugOfWarBar += fightLogic(playerGrade, winner, enemy->scissorsGrade);
 
-  // Remove appropriate hit points from the player an enemy
+	wprintw(result_win, "%s used %s\n", player->name.c_str(), playerWeap.c_str());
+	wprintw(result_win, "%s used %s\n", enemy->name.c_str(), enemy->scissorsName.c_str());
+	wprintw(result_win, "Tug of war bar = %d\n", tugOfWarBar);
+	wrefresh(result_win);
 
-  // If either player or enemy's hit points are zero set the opponent as the victor.
+  } else if (enemy->rockGrade == 0 && enemy->scissorsGrade == 0) { // it is an enemy with only paper
+	p2 = paper;
+	winner = (p1 - p2 + 3) % 3;
+	tugOfWarBar += fightLogic(playerGrade, winner, enemy->paperGrade);
 
-  // End while loop
+  } else if (enemy->paperGrade == 0 && enemy->scissorsGrade == 0) { // it is an enemy with only rock
+	p2 = rock;
+	winner = (p1 - p2 + 3) % 3;
+    tugOfWarBar += fightLogic(playerGrade, winner, enemy->rockGrade);
 
-  // Display the victor to the user
+  } else { // enemy has all weapons choose random
+	p2 = rand() % 3; // computer chooses
+	if (p2 == 0) { // computer chose rock
+	  winner = (p1 - p2 + 3) % 3;
+	  tugOfWarBar += fightLogic(playerGrade, winner, enemy->rockGrade);
 
-  // Update the players money if they are the victor
- 
-  // Return
+	} else if (p2 == 1) { // computer chose paper
+	  winner = (p1 - p2 + 3) % 3;
+	  tugOfWarBar += fightLogic(playerGrade, winner, enemy->paperGrade);
+
+	} else { // computer chose scissors
+	  winner = (p1 - p2 + 3) % 3;
+	  tugOfWarBar += fightLogic(playerGrade, winner, enemy->scissorsGrade);
+
+	}
+  }
+
 }
 
-void Interact::buy(character* merchant) {
+void Interact::buy(character* merchant, int choice) {
 
   // Put weapons for player in an array
 
@@ -162,24 +197,29 @@ void Interact::buy(character* merchant) {
   // return void
 }
 
-void print_menu(WINDOW *menu_win, int highlight)
-{
-	int x, y, i;
-	x = 2;
-	y = 2;
-	box(menu_win, 0, 0);
-	for(i = 0; i < n_choices; ++i)
-	{	if(highlight == i + 1) /* High light the present choice */
-		{	wattron(menu_win, A_REVERSE); 
-			mvwprintw(menu_win, y, x, "%s", choices[i].c_str());
-			wattroff(menu_win, A_REVERSE);
-		}
-		else
-			mvwprintw(menu_win, y, x, "%s", choices[i].c_str());
-		++y;
-	}
-	wrefresh(menu_win);
+void Interact::print_menu(WINDOW *menu_win, int highlight) {
+  int x, y, i;
+  x = 2;
+  y = 2;
+  box(menu_win, 0, 0);
+  for(i = 0; i < n_choices; ++i) {
+    if(highlight == i + 1) {
+      wattron(menu_win, A_REVERSE); 
+      mvwprintw(menu_win, y, x, "%s", choices[i].c_str());
+      wattroff(menu_win, A_REVERSE);
+    } else 
+      mvwprintw(menu_win, y, x, "%s", choices[i].c_str());
+    ++y;
+  }
+  wrefresh(menu_win);
 }
+
+void Interact::print_results(WINDOW *result_win) {
+  box(result_win,0,0);
+  mvwprintw(result_win,1,1,"Let's fight!\n");
+  wrefresh(result_win);
+}
+
 
 void Interact::setOptions(character* options) {
   int price = (options->rockGrade -1) * 100;
@@ -188,15 +228,16 @@ void Interact::setOptions(character* options) {
   
   if(options->type == "Vendor") {
 	n_choices = 4;
-	choices[0] = options->rockName + priceString.str();
-	choices[1] = options->paperName + priceString.str();
-	choices[2] = options->scissorsName + priceString.str();
+	choices[0] = options->rockName +  "$" + priceString.str();
+	choices[1] = options->paperName + "$" + priceString.str();
+	choices[2] = options->scissorsName + "$" + priceString.str();
 	choices[3] = "Exit";
 
   } else {
 	n_choices = 3;
-	choices[0] = options->rockName;
-	choices[1] = options->paperName;
-	choices[2] = options->scissorsName;
+	choices[0] = player->rockName;
+	choices[1] = player->paperName;
+	choices[2] = player->scissorsName;
   }
 }
+
